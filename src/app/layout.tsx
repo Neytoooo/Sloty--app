4418d884d54c8c5dd6d2b99e4768ef4dbd31c0c3
Mattis <mattiskhn@gmail.com>
2026-09-23 15:51:32 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
+import { Urbanist } from 'next/font/google'
+
+const urbanist = Urbanist({
+  subsets: ['latin'],
+  variable: '--font-urbanist',
+})
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
-      <html lang="fr">
-        <body>{children}</body>
+      <html lang="fr" className={urbanist.variable}>
+        <body className="font-sans">{children}</body>
       </html>
     </ClerkProvider>
   )
